f1a335adc23b7daa462d5be11f2f834bf0db4bf4
omjadhav3714 <omjadhav963@gmail.com>
2026-06-12 15:13:38 +0530

    feat: env added

--- db.js
+++ db.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/workshop')
+const dotenv = require('dotenv');
+
+dotenv.config(); // Load environment variables from .env file
+
+mongoose.connect(process.env.DB_URL)
     .then(() => console.log('DB Connected')); // 3. Create Schema & Model
 
 module.exports = {
